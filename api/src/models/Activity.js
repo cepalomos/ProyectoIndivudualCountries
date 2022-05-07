aebab5b9b82a487cf5dabb58e685172092a5b953
Carlos Palomo Serna <cepalomos@outlook.es>
2022-05-07 11:16:02 -0500

    primer commit del proyecto

---
+++ api/src/models/Activity.js
@@ -0,0 +1,32 @@
+const {DataTypes} = require('sequelize');
+
+module.exports = (sequelize) =>{
+    sequelize.define('activity',{
+        id:{
+            type:DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull:false,
+            primaryKey: true
+        },
+        nombre:{
+            type:DataTypes.STRING
+        },
+        dificultad:{
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+                len: [1,5],
+                isNumeric: true,
+                min: 1,
+                max: 5
+            },
+        },
+        duracion:{
+            type:DataTypes.TIME,
+
+        },
+        temporada: {
+            type: DataTypes.ENUM('verano', 'otono', 'invierno','primavera')
+          },
+    })
+}
---
+++ api/src/models/Country.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('country', {
+    id:{
+      type:DataTypes.STRING,
+      primaryKey:true,
+      unique:true
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    imagen:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    continente:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    capital:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    subregion:{
+      type:DataTypes.STRING,
+    },
+    area:{
+      type:DataTypes.DOUBLE,
+    },
+    poblacion:{
+      type:DataTypes.DOUBLE,
+    },
+  });
+};
